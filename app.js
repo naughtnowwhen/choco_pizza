a59a7a617fbc864e1556bcbb61acee2c9d9d436f
Peter <naughtnowwhen@gmail.com>
2018-07-20 18:15:08 -0700

    no change

---
+++ app.js
@@ -0,0 +1,15 @@
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/flic-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/gp-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/insta-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/list-bg.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/logo.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/mail-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/pint-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/print-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/rss-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/small-logo.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/twit-icon.png>"
+<img src="/Users/petermurphy/Desktop/choco-pizza-folder/van-pic.png>"
+
+
+  
